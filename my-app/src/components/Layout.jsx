bc8e8f2ef53c8f3db1df4c10b6884ef35d24f5f4
Sun English <sunenglishjr@gmail.com>
2024-11-29 15:39:32 -0500

    layout in progress

---
+++ my-app/src/components/Layout.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+
+import { Grid, GridItem, Box, Heading, Text, Tabs} from "@chakra-ui/react";
+import AddResourceDrawer from "./AddResourceDrawer";
+import SignUp from "./SignUp";
+import ResourceList from "./ResourceList";
+import { ColorModeButton } from "./ui/color-mode"; // Adjust the path if necessary
+
+const Layout = () => {
+    const [value, setValue] = useState("first")
+
+  return (
+    <>
+      <ColorModeButton />
+      <Tabs.Root 
+      value={value} 
+      onValueChange={(e) => setValue(e.value)}
+      
+      >
+      <Tabs.List style={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}>
+        <Tabs.Trigger value="first">First tab</Tabs.Trigger>
+        <Tabs.Trigger value="second">Second tab</Tabs.Trigger>
+        <Tabs.Trigger value="third">Third tab</Tabs.Trigger>
+        <Tabs.Trigger value="fourth">Fourth tab</Tabs.Trigger>
+        <Tabs.Trigger value="fifth">Fifth tab</Tabs.Trigger>
+      </Tabs.List>
+
+      <Tabs.Content value="first">
+        <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={8}>
+          <GridItem>
+            <Box>
+              <Heading as="h3">Add Resource</Heading>
+              <AddResourceDrawer />
+            </Box>
+          </GridItem>
+          <GridItem>
+            <Box>
+              <Heading as="h3">Resource List</Heading>
+              <ResourceList />
+            </Box>
+          </GridItem>
+        </Grid>
+      </Tabs.Content>
+      <Tabs.Content value="second">
+        <Box>
+          <Heading as="h3">Sign Up</Heading>
+          <SignUp />
+        </Box>
+      </Tabs.Content>
+      <Tabs.Content value="third">
+        <Text>Another additional content</Text>
+      </Tabs.Content>
+      <Tabs.Content value="fourth">
+        <Text>Another additional content</Text>
+      </Tabs.Content>
+      <Tabs.Content value="fifth">
+        <Text>Another additional content</Text>
+      </Tabs.Content>
+    </Tabs.Root>
+  </>
+  );
+};
+
+export default Layout;
