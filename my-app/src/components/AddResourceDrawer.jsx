62b06c7cff4f42177caed329441f7b2feb2f6988
Sun English <sunenglishjr@gmail.com>
2024-11-29 10:19:54 -0500

    add drawer field

--- my-app/src/components/AddResourceDrawer.jsx
+++ my-app/src/components/AddResourceDrawer.jsx
@@ -36,6 +36,63 @@ const resourceTypes = createListCollection({
   ],
 });
 
+const states = createListCollection({
+    items: [
+      { label: "Alabama", value: "AL" },
+      { label: "Alaska", value: "AK" },
+      { label: "Arizona", value: "AZ" },
+      { label: "Arkansas", value: "AR" },
+      { label: "California", value: "CA" },
+      { label: "Colorado", value: "CO" },
+      { label: "Connecticut", value: "CT" },
+      { label: "Delaware", value: "DE" },
+      { label: "Florida", value: "FL" },
+      { label: "Georgia", value: "GA" },
+      { label: "Hawaii", value: "HI" },
+      { label: "Idaho", value: "ID" },
+      { label: "Illinois", value: "IL" },
+      { label: "Indiana", value: "IN" },
+      { label: "Iowa", value: "IA" },
+      { label: "Kansas", value: "KS" },
+      { label: "Kentucky", value: "KY" },
+      { label: "Louisiana", value: "LA" },
+      { label: "Maine", value: "ME" },
+      { label: "Maryland", value: "MD" },
+      { label: "Massachusetts", value: "MA" },
+      { label: "Michigan", value: "MI" },
+      { label: "Minnesota", value: "MN" },
+      { label: "Mississippi", value: "MS" },
+      { label: "Missouri", value: "MO" },
+      { label: "Montana", value: "MT" },
+      { label: "Nebraska", value: "NE" },
+      { label: "Nevada", value: "NV" },
+      { label: "New Hampshire", value: "NH" },
+      { label: "New Jersey", value: "NJ" },
+      { label: "New Mexico", value: "NM" },
+      { label: "New York", value: "NY" },
+      { label: "North Carolina", value: "NC" },
+      { label: "North Dakota", value: "ND" },
+      { label: "Ohio", value: "OH" },
+      { label: "Oklahoma", value: "OK" },
+      { label: "Oregon", value: "OR" },
+      { label: "Pennsylvania", value: "PA" },
+      { label: "Rhode Island", value: "RI" },
+      { label: "South Carolina", value: "SC" },
+      { label: "South Dakota", value: "SD" },
+      { label: "Tennessee", value: "TN" },
+      { label: "Texas", value: "TX" },
+      { label: "Utah", value: "UT" },
+      { label: "Vermont", value: "VT" },
+      { label: "Virginia", value: "VA" },
+      { label: "Washington", value: "WA" },
+      { label: "West Virginia", value: "WV" },
+      { label: "Wisconsin", value: "WI" },
+      { label: "Wyoming", value: "WY" },
+    ],
+      
+  })
+
+
 const AddResourceDrawer = () => {
   const {
     register,
@@ -185,6 +242,36 @@ const AddResourceDrawer = () => {
                   {...register("city", { required: "City is required" })}
                   />
               </Field>
+
+              {/* Resource State Field */}
+              <Field
+                label="State"
+                invalid={!!errors.state}
+                errorText={errors.state?.message}
+                required
+                >   
+                <SelectRoot
+                  value={watch("state")}
+                  onValueChange={(value) => setValue("state", value)}
+                  collection={states}
+                >
+                    <SelectTrigger>
+                      <SelectValueText placeholder="Select state" />
+                    </SelectTrigger>
+
+                    <SelectContent zIndex="1500">
+                        <SelectItem item={{ label: "Select State", value: "" }} key="" />
+                        {states.items.map((state) => (
+                          <SelectItem item={state} key={state.value}>
+                            {state.label}
+                          </SelectItem>
+                        ))}
+  
+                    </SelectContent>
+
+                    </SelectRoot>
+                    
+                 </Field>
 
 
               {/* Zip Code Field */}
