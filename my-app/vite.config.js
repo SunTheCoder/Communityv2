d0bc2a3f7e3a67a7f75fa81b56ae83e5a354f67b
Sun English <sunenglishjr@gmail.com>
2025-02-12 09:53:17 -0500

    deploy edit

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from "vite-jsconfig-paths";
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, 'src'),
       '@resolvers/zod': '@hookform/resolvers/zod',
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@ui': path.resolve(__dirname, 'src/components/ui'),
     },
   },
   server: {
